73b67d2985d5aa6fbfd58d5130f0f76c6df33832
Fiuxer <fiuxerpro@gmail.com>
2024-06-02 23:28:49 -0600

    codifica

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -28,19 +28,392 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label14 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown10 = new System.Windows.Forms.NumericUpDown();
+            this.label15 = new System.Windows.Forms.Label();
+            this.numericUpDown11 = new System.Windows.Forms.NumericUpDown();
+            this.label16 = new System.Windows.Forms.Label();
+            this.numericUpDown12 = new System.Windows.Forms.NumericUpDown();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            this.label10 = new System.Windows.Forms.Label();
+            this.numericUpDown8 = new System.Windows.Forms.NumericUpDown();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(38, 430);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(108, 13);
+            this.label14.TabIndex = 65;
+            this.label14.Text = "Calificación Parcial 3:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(41, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 64;
+            this.button3.Text = "Calcular";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown10
+            // 
+            this.numericUpDown10.Location = new System.Drawing.Point(231, 370);
+            this.numericUpDown10.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown10.Name = "numericUpDown10";
+            this.numericUpDown10.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown10.TabIndex = 63;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(41, 372);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(66, 13);
+            this.label15.TabIndex = 62;
+            this.label15.Text = "Cal. Examen";
+            // 
+            // numericUpDown11
+            // 
+            this.numericUpDown11.Location = new System.Drawing.Point(231, 306);
+            this.numericUpDown11.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown11.Name = "numericUpDown11";
+            this.numericUpDown11.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown11.TabIndex = 61;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(41, 308);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(107, 13);
+            this.label16.TabIndex = 60;
+            this.label16.Text = "Trabajos entregados:";
+            // 
+            // numericUpDown12
+            // 
+            this.numericUpDown12.Location = new System.Drawing.Point(231, 283);
+            this.numericUpDown12.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown12.Name = "numericUpDown12";
+            this.numericUpDown12.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown12.TabIndex = 59;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(41, 285);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(161, 13);
+            this.label17.TabIndex = 58;
+            this.label17.Text = "Trabajos pedidos por el Maestro:";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(126, 260);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(48, 13);
+            this.label18.TabIndex = 57;
+            this.label18.Text = "Parcial 3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(478, 177);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 13);
+            this.label8.TabIndex = 54;
+            this.label8.Text = "Calificación Parcial 2:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(481, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 53;
+            this.button2.Text = "Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(671, 117);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown6.TabIndex = 52;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(481, 119);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 13);
+            this.label9.TabIndex = 51;
+            this.label9.Text = "Cal. Examen";
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(671, 53);
+            this.numericUpDown7.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown7.TabIndex = 50;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(481, 55);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(107, 13);
+            this.label10.TabIndex = 49;
+            this.label10.Text = "Trabajos entregados:";
+            // 
+            // numericUpDown8
+            // 
+            this.numericUpDown8.Location = new System.Drawing.Point(671, 30);
+            this.numericUpDown8.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown8.Name = "numericUpDown8";
+            this.numericUpDown8.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown8.TabIndex = 48;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(481, 32);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(161, 13);
+            this.label11.TabIndex = 47;
+            this.label11.Text = "Trabajos pedidos por el Maestro:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(566, 7);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 13);
+            this.label12.TabIndex = 46;
+            this.label12.Text = "Parcial 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 13);
+            this.label6.TabIndex = 43;
+            this.label6.Text = "Calificación Parcial 1:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 42;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(203, 117);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 41;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Cal. Examen";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(203, 53);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 39;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Trabajos entregados:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(203, 30);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 37;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 13);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Trabajos pedidos por el Maestro:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Parcial 1";
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown10);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.numericUpDown11);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.numericUpDown12);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.numericUpDown7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.numericUpDown8);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Name = "Form4";
             this.Text = "Codifica Software";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown10;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.NumericUpDown numericUpDown11;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.NumericUpDown numericUpDown12;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.NumericUpDown numericUpDown8;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,186 @@ namespace Proyecto
         public Form4()
         {
             InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Consigue el numero de trabajos que se ingreso en el menu
+            int trabajos = Convert.ToInt32(numericUpDown1.Value);
+
+            ///Declara las Variables
+            int pt_e;
+            double temporal;
+            double[] tra = new double[trabajos];
+            double tra_c = 0;
+            double tra_p = Convert.ToDouble(numericUpDown1.Value);
+            double tra_e = Convert.ToDouble(numericUpDown2.Value);
+            double exa_c = 0;
+
+            // Evita que se entregen mas trabajos de las que se pidieron
+            if (numericUpDown2.Value > numericUpDown1.Value)
+            {
+                numericUpDown2.Value = numericUpDown1.Value;
+            }
+
+            //Pide la calificación de cada una de las practicas
+            for (int i = 0; i < trabajos; i++)
+            {
+
+                // Pide la calificación de la practica "i"
+                temporal = Convert.ToDouble(Interaction.InputBox("Ingrese la calificación del trabajo numero: " + (i + 1)));
+
+
+                // Si la calificación es una real, la guarda en la variable "tar"
+                if (temporal < 11 && temporal > -1)
+                {
+                    tra[i] = (temporal);
+                    tra_c += tra[i];
+                }
+
+                // Si no, le vuelve a pedir un numero que sea valido
+                else
+                {
+                    MessageBox.Show("Ingrese un valor valido.");
+                    i = i - 1;
+                }
+
+
+            }
+
+            tra_c = (tra_c / trabajos) * 0.5;
+            exa_c = (Convert.ToDouble(numericUpDown3.Value)) * 0.5;
+
+            if ((tra_c + exa_c) < 6)
+            {
+                label6.ForeColor = Color.Red;
+            }
+            else
+            {
+                label6.ForeColor = SystemColors.ControlText;
+            }
+            label6.Text = "Calificación Parcial 1: " + ((tra_c + exa_c)) + " / 10";
+            MessageBox.Show(label6.Text, "Resultado");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //Consigue el numero de trabajos que se ingreso en el menu
+            int trabajos = Convert.ToInt32(numericUpDown8.Value);
+
+            ///Declara las Variables
+            double temporal;
+            double[] tra = new double[trabajos];
+            double tra_c = 0;
+            double tra_p = Convert.ToDouble(numericUpDown8.Value);
+            double tra_e = Convert.ToDouble(numericUpDown7.Value);
+            double exa_c = 0;
+
+            // Evita que se entregen mas trabajos de las que se pidieron
+            if (numericUpDown7.Value > numericUpDown8.Value)
+            {
+                numericUpDown7.Value = numericUpDown8.Value;
+            }
+
+            //Pide la calificación de cada una de las practicas
+            for (int i = 0; i < trabajos; i++)
+            {
+
+                // Pide la calificación de la practica "i"
+                temporal = Convert.ToDouble(Interaction.InputBox("Ingrese la calificación del trabajo numero: " + (i + 1)));
+
+
+                // Si la calificación es una real, la guarda en la variable "tar"
+                if (temporal < 11 && temporal > -1)
+                {
+                    tra[i] = (temporal);
+                    tra_c += tra[i];
+                }
+
+                // Si no, le vuelve a pedir un numero que sea valido
+                else
+                {
+                    MessageBox.Show("Ingrese un valor valido.");
+                    i = i - 1;
+                }
+
+
+            }
+
+            tra_c = (tra_c / trabajos) * 0.5;
+            exa_c = (Convert.ToDouble(numericUpDown6.Value)) * 0.5;
+
+            if ((tra_c + exa_c) < 6)
+            {
+                label8.ForeColor = Color.Red;
+            }
+            else
+            {
+                label8.ForeColor = SystemColors.ControlText;
+            }
+            label8.Text = "Calificación Parcial 3: " + ((tra_c + exa_c)) + " / 10";
+            MessageBox.Show(label8.Text, "Resultado");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //Consigue el numero de trabajos que se ingreso en el menu
+            int trabajos = Convert.ToInt32(numericUpDown12.Value);
+
+            ///Declara las Variables
+            double temporal;
+            double[] tra = new double[trabajos];
+            double tra_c = 0;
+            double tra_p = Convert.ToDouble(numericUpDown12.Value);
+            double tra_e = Convert.ToDouble(numericUpDown11.Value);
+            double exa_c = 0;
+
+            // Evita que se entregen mas trabajos de las que se pidieron
+            if (numericUpDown11.Value > numericUpDown12.Value)
+            {
+                numericUpDown11.Value = numericUpDown12.Value;
+            }
+
+            //Pide la calificación de cada una de las practicas
+            for (int i = 0; i < trabajos; i++)
+            {
+
+                // Pide la calificación de la practica "i"
+                temporal = Convert.ToDouble(Interaction.InputBox("Ingrese la calificación del trabajo numero: " + (i + 1)));
+
+
+                // Si la calificación es una real, la guarda en la variable "tar"
+                if (temporal < 11 && temporal > -1)
+                {
+                    tra[i] = (temporal);
+                    tra_c += tra[i];
+                }
+
+                // Si no, le vuelve a pedir un numero que sea valido
+                else
+                {
+                    MessageBox.Show("Ingrese un valor valido.");
+                    i = i - 1;
+                }
+
+
+            }
+
+            tra_c = (tra_c / trabajos) * 0.5;
+            exa_c = (Convert.ToDouble(numericUpDown10.Value)) * 0.5;
+
+            if ((tra_c + exa_c) < 6)
+            {
+                label14.ForeColor = Color.Red;
+            }
+            else
+            {
+                label14.ForeColor = SystemColors.ControlText;
+            }
+            label14.Text = "Calificación Parcial 3: " + ((tra_c + exa_c)) + " / 10";
+            MessageBox.Show(label14.Text, "Resultado");
         }
     }
 }
